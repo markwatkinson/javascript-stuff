6ea0d3c501c1dabe7ec1748be24251315a3bd2ca
Mark Watkinson <markwatkinson@gmail.com>
2011-02-17 00:36:30 +0000

    Make roundify work with FF3.6

--- src/jquery.roundify/jquery.roundify.js
+++ src/jquery.roundify/jquery.roundify.js
@@ -7,17 +7,31 @@
       width: '1px'
     }, options);
     
-    this.each(function(i, e) {
-      var $this = $(this);      
-      $this.css( {
-        'border-color': options.color,
-        'border-width': options.width,
-        'border-style': 'solid',
-        '-moz-border-radius' : options.radius,
-        '-webkit-border-radius' : options.radius,
-        'border-radius' : options.radius,
-        'overflow' : 'hidden'
-      });
+    this.each(function(i, e) {      
+      var $this = $(this),              
+          $wrap = $('<span>').css({
+            'background-image': 'url(' + $this.attr('src') + ')',
+            'border-color': options.color,
+            'border-width': options.width,
+            'border-style': 'solid',
+            '-moz-border-radius' : options.radius,
+            '-webkit-border-radius' : options.radius,
+            'border-radius' : options.radius,
+            'overflow' : 'hidden',
+            'display' : 'inline-block',
+            'background-repeat': 'no-repeat'
+          });
+          if ($this.is('img')) {            
+            var handler = function() {
+              $this.parent().css('height', $this.height() + 'px');
+            };
+            
+            $this.wrap($wrap);            
+            $this.load(handler);
+            $this.resize(handler);
+            $this.css('opacity', '0');
+          }
+    
     });
     return this;
   }
