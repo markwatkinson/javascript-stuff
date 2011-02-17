6541fccf3ace0d22fe1a0313dd95264583191c38
Mark Watkinson <markwatkinson@gmail.com>
2011-02-17 01:04:20 +0000

    Fix roundify to now be generally useful on browsers supporting border radius

--- src/jquery.roundify/jquery.roundify.js
+++ src/jquery.roundify/jquery.roundify.js
@@ -21,16 +21,23 @@
             'display' : 'inline-block',
             'background-repeat': 'no-repeat'
           });
-          if ($this.is('img')) {            
-            var handler = function() {
-              $this.parent().css('height', $this.height() + 'px');
-            };
-            
-            $this.wrap($wrap);            
-            $this.load(handler);
-            $this.resize(handler);
-            $this.css('opacity', '0');
-          }
+      if ($this.is('img')) {            
+        var handler = function() {
+          $wrap.css('height', $this.height() + 'px');
+        };
+        
+        $this.wrap($wrap);
+        // somehow $wrap now does not refer to anything useful.
+        $wrap = $this.parent();
+        // sometimes IE won't fire this for some reason, it may be that 
+        // it won't refire load if it's already occurred, I don't know, 
+        // but we need to fire it at least once to get the box the right 
+        // size.
+        handler();
+        $this.load(handler);
+        $this.resize(handler);
+        $this.css('opacity', '0');
+      }
     
     });
     return this;
