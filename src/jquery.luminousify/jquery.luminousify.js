ce4fb45633621d0b9f23709e448da381947e9c57
Mark Watkinson <markwatkinson@gmail.com>
2011-02-23 19:14:47 +0000

    Adapt luminousify to group together multiple highlight requests into one HTTP request

--- src/jquery.luminousify/jquery.luminousify.js
+++ src/jquery.luminousify/jquery.luminousify.js
@@ -14,7 +14,11 @@
         customStyle: '',
         inline: false,
         path: '/luminous',
-        line_numbers: false
+        line_numbers: false,
+        group: 10 // number of code snippets to group into one request.
+                 // This reduces the HTTP overhead but also ties up the server 
+                 // for longer at once. 5-10 is resaonable.
+                 // server side maximum is 20.
       }, options);
     
     if (!__included) {
@@ -42,33 +46,55 @@
 
     }
     
-    var exec = function() {      
+    var exec = function() {
       if (i >= $t.length)
         return;
-      var t = $($t.get(i));
-      var code = t.html();
-      var language = options.language;
-      if (!language)
-        language = t.attr('lang');
-      if (!language) 
-        language = options.defaultLanguage;
-      if (!language)
-        language = '';
+      
+      var j;
+      var reqs = [];
+      for (j=0; j < options.group; j++) {
+        if (j+i >= $t.length)
+          break;
+        var t = $($t.get(i+j));
+        var code = t.html();
+
+
+        var language = options.language;
+        if (!language)
+          language = t.attr('lang');
+        if (!language) 
+          language = options.defaultLanguage;
+        if (!language)
+          language = '';        
+        reqs.push( { code: code, language: language, line_numbers: options.line_numbers,
+                   inline: options.inline, escaped: true} );
+      }
+      
+      
+      
+      
       
       $.post(options.url,
-        {
-          code: code, 
-          language: language,
-          line_numbers: options.line_numbers, 
-          inline: options.inline,
-          escaped: true
-        }, 
+             {
+               requests: reqs
+             },
+        
         function(data) {
-          var $r = $(data)
-          if (!options.inline)
-            $r.addClass('luminousified');
-          t.replaceWith($r);
-          i++;
+          // the return is JSON encoded, but we know it's just an array of 
+          // strings. To avoid depending on a JSON parser we're going to 
+          // regex extract them
+          var strings = data.match(/"([^\\"]+|\\"|\\.)*"/g);
+          for (var a = 0; a<strings.length; a++) {
+            // we KNOW anything in here is not executable code because it's 
+            // wrapped in quotes. It may contain escape sequences. The easiest
+            // way to decode these therefore is to eval() it. This is safe.       
+            var s = eval(strings[a]); 
+            var $r = $(s);
+            if (!options.inline)
+              $r.addClass('luminousified');
+            $($t.get(i)).replaceWith($r);
+            i++;
+          }
           setTimeout(exec, options.delay);
         });
     };
